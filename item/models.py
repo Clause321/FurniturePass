a51df0c2a74458db500d1d0b167a2e426cd1ba64
renl <renl@umich.edu>
2015-01-23 17:09:26 -0500

    ignore idea configs

--- item/models.py
+++ item/models.py
@@ -1,7 +1,14 @@
-from user.models import *
+from django.db import models
+import user
+
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True, default=0)
+    tag_name = models.CharField(unique=True, max_length=20)
 
 class Item(models.Model):
-    '''owner = models.ForeignKey(MyUser)
+    owner = models.ForeignKey('user.MyUser')
+    #buyer = models.ForeignKey('user.MyUser') <= cannot write like this because django cannot tell
+    final_buyer = models.ForeignKey('user.MyUser', related_name='MyUser')
     item_id = models.AutoField(primary_key=True, default=0)
     self_price = models.IntegerField()
     sell_time = models.DateTimeField()
@@ -10,20 +17,7 @@ class Item(models.Model):
     status = models.CharField(max_length=30)
     expire_time = models.DateTimeField()
     tag = models.ManyToManyField(Tag)
-    #
-    final_buyer = models.ForeignKey(MyUser, null=True)
-'''
-    pass
-
-class Tag(models.Model):
-    '''tag_id = models.AutoField(primary_key=True, default=0)
-    tag_name = models.CharField(unique=True, max_length=20)
-    '''
-    pass
 
 class Category(models.Model):
-    '''
     catagory_id = models.AutoField(primary_key=True, default=0)
     catagory_name = models.CharField(max_length=40)
-    '''
-    pass
